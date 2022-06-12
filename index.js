e0ca4ff5f0f44d243461c59d5dccaeb5766d5225
ataashii <80128801+Ataashii@users.noreply.github.com>
2022-06-12 17:02:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ client.on('ready', () => {
     setInterval(sync, 15000);
 
     const { version } = require('./package.json');
-    client.user.setActivity(`Vinted BOT | v${version}`);
+    client.user.setActivity(`Vinted BOT | v${version} | by vqzr#5411`);
 });
 
 client.on('interactionCreate', (interaction) => {
